dffc9ffece528ef4375233fe53591121961a50dd
Ethan <79857871+EthanGely@users.noreply.github.com>
2022-10-10 12:05:22 +0200

    Add files via upload

---
+++ src/include/gabarit_site/recherche.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>YAKAK</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<header>
+    <h1>YAKAK - Où vous voulez, mais en Kayak (c'est plus écolo)</h1>
+
+    <div class="menu">
+        <h2>Menu</h2>
+        <ul>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Destinations</a></li>
+            <li><a href="">Visite vitruelle (audio)</a></li>
+            <li><a href="">Visite virtuelle (vidéo)</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</header>
+
+<div class="content">
+    <h3>Promotion</h3>
+
+    <div class="offresDest">
+        <table>
+            <thead>
+            <tr>
+                <td>Destination</td>
+                <td>Offre</td>
+                <td>Prix</td>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>Espagne<img src="../images/espagne.jpeg" alt="Espagne"></td>
+                <td>Circuit plage, hôtel 4 *</td>
+                <td>800 €</td>
+                <td><button>Découvrir l'offre</button></td>
+            </tr>
+            <tr>
+                <td>Maroc<img src="../images/maroc.jpg" alt="Maroc"></td>
+                <td>Circuit Oasis, hôtel 4*</td>
+                <td>1000 €</td>
+                <td><button>Découvrir l'offre</button></td>
+            </tr>
+            <tr>
+                <td>Brésil<img src="../images/bresil.jpg" alt="Brésil"></td>
+                <td>Circuit Favellas, hôtel 4*</td>
+                <td>2000 €</td>
+                <td><button>Découvrir l'offre</button></td>
+            </tr>
+            <tr>
+                <td>Japon<img src="../images/mont_fuji.jpg" alt="Japon"></td>
+                <td>Circuit Fuji, hôtel 4*</td>
+                <td>2500 €</td>
+                <td><button>Découvrir l'offre</button></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+<footer>
+    <p>this is a footer</p>
+</footer>
+
+
+</body>
+</html>
